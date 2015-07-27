b9538dfcbb289563dae3f244fde8ba5df728ee54
Jeison Solarte <jeisonsolarte@gmail.com>
2015-07-27 10:36:46 -0500

    listas actividades base

---
+++ app/src/main/java/co/edu/sena/jonathan/events2015/SplashActivity.java
@@ -0,0 +1,33 @@
+package co.edu.sena.jonathan.events2015;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity implements Runnable{
+
+    private final int TIME_THREAD=2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread=new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(TIME_THREAD);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent i=new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(i);
+    }
+}
