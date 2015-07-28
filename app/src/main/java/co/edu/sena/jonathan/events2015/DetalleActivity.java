9d880d3525bd5dd50644c2b56f34b380812e1f28
Jeison Solarte <jeisonsolarte@gmail.com>
2015-07-27 19:38:36 -0500

    listo evento de notificación

--- app/src/main/java/co/edu/sena/jonathan/events2015/DetalleActivity.java
+++ app/src/main/java/co/edu/sena/jonathan/events2015/DetalleActivity.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.plus.People;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
 
 
 public class DetalleActivity extends Activity {
 
+    private MixpanelAPI mixpanelAPI;
+    EventsApplication application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +24,11 @@ public class DetalleActivity extends Activity {
 
     }
 
+
+    public void submitNotification(View view) {
+        mixpanelAPI=MixpanelAPI.getInstance(this,"33fda110df511f50c5eed8a357b1d44c");
+        MixpanelAPI.People people= mixpanelAPI.getPeople();
+        people.identify("usuarioWS"+application.getListaEventos().get(0).getNombre());
+        people.initPushHandling("214490295855");
+    }
 }
